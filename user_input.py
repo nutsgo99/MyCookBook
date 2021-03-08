89e606c1e166c50aacbed276129b390eea8aef36
NutsGo99 <79571949+nutsgo99@users.noreply.github.com>
2021-03-08 22:16:45 +0000

    Adding HelloWorld

---
+++ user_input.py
@@ -0,0 +1,8 @@
+print("Hello")
+name = input("What is your name? ")
+print("Hello " + name )
+
+number = input("What is your favourite number? ")
+triple = int(number) * 3
+print("Your number multiplied by three is " + str(triple) + " ")
+
